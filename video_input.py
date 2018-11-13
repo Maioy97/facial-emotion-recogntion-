9efa3a192063d64b238c255b6aebd99c272c3967
HayaLababidi <38756711+HayaLababidi@users.noreply.github.com>
2018-11-13 18:05:28 +0200

    added dataset jason file

--- video_input.py
+++ video_input.py
@@ -5,18 +5,19 @@ import facedetecion
 
 cascPath = 'HAAR_xmls/haarcascade_frontalface_default.xml'
 faceCascade = cv2.CascadeClassifier(cascPath)
-video = 'videos/short/angry male 2.mp4'
+video = '../videos/short/sad Female.mp4'
 video_capture = cv2.VideoCapture(video)
 
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
-
+    if ret == 0:
+        break
     frame = facedetecion.detect(frame)
     # Display the resulting frame
     cv2.imshow('Video', frame)
 
-    if cv2.waitKey(25) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # When everything is done, release the capture
