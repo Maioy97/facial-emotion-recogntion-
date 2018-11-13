745e92ff4cd4f968a314552dd4304b684eedb2bf
HayaLababidi <38756711+HayaLababidi@users.noreply.github.com>
2018-11-13 18:25:27 +0200

    input option

--- video_input.py
+++ video_input.py
@@ -5,14 +5,21 @@ import facedetecion
 
 cascPath = 'HAAR_xmls/haarcascade_frontalface_default.xml'
 faceCascade = cv2.CascadeClassifier(cascPath)
-video = '../videos/short/sad Female.mp4'
-video_capture = cv2.VideoCapture(video)
+videopath = '../videos/short/'
+videoType=input("videoType")
+if videoType=='cam':
+    video_capture = cv2.VideoCapture(0)
+else:
+    video_capture = cv2.VideoCapture(videopath+videoType)
 
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
     if ret == 0:
         break
+    height, width, channels = frame.shape
+    if width>1000:
+        frame = cv2.resize(frame, (int(width/3), int(height/3)))
     frame = facedetecion.detect(frame)
     # Display the resulting frame
     cv2.imshow('Video', frame)
